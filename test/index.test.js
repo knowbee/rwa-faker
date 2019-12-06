f99a0dac0b6ddd7ab6c3b07e2e63d08d91272340
Googler <knowbeeinc@gmail.com>
2019-12-06 12:18:06 +0200

    added more tests

--- test/index.test.js
+++ test/index.test.js
@@ -8,11 +8,29 @@ describe("rwafaker", async () => {
       const name = rwafaker.firstName();
       expect(name).to.be.a("string");
     });
+    it("should return 5 random first names", function() {
+      const name = rwafaker.firstName(5);
+      expect(name).to.be.a("array");
+      expect(name.length).to.equal(5);
+    });
+    it("should fail", function() {
+      const name = rwafaker.firstName("5");
+      expect(name).to.equal("failed, wrong input");
+    });
   });
   describe("generate random last name", () => {
     it("should return a random last name", function() {
       const name = rwafaker.lastName();
       expect(name).to.be.a("string");
+    });
+    it("should return 5 random last names", function() {
+      const name = rwafaker.lastName(5);
+      expect(name).to.be.a("array");
+      expect(name.length).to.equal(5);
+    });
+    it("should fail", function() {
+      const name = rwafaker.lastName("5");
+      expect(name).to.equal("failed, wrong input");
     });
   });
   describe("generate random email", () => {
@@ -20,11 +38,29 @@ describe("rwafaker", async () => {
       const email = rwafaker.email();
       expect(email).to.be.a("string");
     });
+    it("should return 5 random emails", function() {
+      const email = rwafaker.email(5);
+      expect(email).to.be.a("array");
+      expect(email.length).to.equal(5);
+    });
+    it("should fail", function() {
+      const email = rwafaker.email("5");
+      expect(email).to.equal("failed, wrong input");
+    });
   });
   describe("generate full name", () => {
     it("should return a random fullname", function() {
       const full_name = rwafaker.fullName();
       expect(full_name).to.be.a("string");
+    });
+    it("should return 5 random fullnames", function() {
+      const full_name = rwafaker.fullName(5);
+      expect(full_name).to.be.a("array");
+      expect(full_name.length).to.equal(5);
+    });
+    it("should fail", function() {
+      const full_name = rwafaker.fullName("5");
+      expect(full_name).to.equal("failed, wrong input");
     });
   });
 });
